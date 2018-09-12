6d3c46a40d18f444a6c11dd28a0a6128e8f56df9
zhuweiyou <i@zhuweiyou.com>
2018-09-12 12:00:15 +0800

    头七

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ import Alipay from './component/Alipay';
 import './globalStyle';
 import './hmt';
 
+const Tou7 = () => {
+  const date = new Date();
+  if (date.getDate()) {
+    return (
+      <div>
+        <h1 style={{color: '#dd2323', textAlign: 'center'}}>mtdhb 头七纪念日</h1>
+        <img src={require('./static/tou7.gif')} style={{width: '100%', marginBottom: 30}} alt="头七" />
+      </div>
+    );
+  }
+  return null;
+};
+
 export default () => (
   <div
     style={{
@@ -20,6 +33,7 @@ export default () => (
       bordered={false}
       style={{maxWidth: 640, width: '100%', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)'}}
     >
+      <Tou7 />
       <p>感谢大家大半年以来的支持。</p>
       <p>由于不可描述的原因，本站正式宣布关闭。</p>
       <p>
